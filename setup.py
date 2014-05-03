13e96cfd0e38aeb786c1bfc28aa3af55eeda32bd
40123253 <40123253@gm.nfu.edu.tw>
2014-05-03 13:39:20 +0800

    change name 2014cdbg13

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='YourAppName',
+      version='1.0',
+      description='OpenShift App',
+      author='2014cdbg13',
+      author_email='40123231@gm.nfu.edu.tw',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['CherryPy'],
+     )
